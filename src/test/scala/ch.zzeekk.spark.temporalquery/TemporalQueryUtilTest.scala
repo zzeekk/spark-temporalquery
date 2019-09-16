044e46500242cfd6f52c2f9fbd6f610f9b82db20
Nikolaus Thiel <nikolaus.thiel@sbb.ch>
2019-09-16 16:53:51 +0200

    test cases for temporalCombine and null values

--- src/test/scala/ch.zzeekk.spark.temporalquery/TemporalQueryUtilTest.scala
+++ src/test/scala/ch.zzeekk.spark.temporalquery/TemporalQueryUtilTest.scala
@@ -1,12 +1,12 @@
 package ch.zzeekk.spark.temporalquery
 
 import java.sql.Timestamp
-import org.apache.spark.sql._
-import TemporalQueryUtil._
-import TestUtils._
+import org.apache.spark.sql.{Row, _}
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.types._
 import scala.collection.JavaConverters._
+import TemporalQueryUtil._
+import TestUtils._
 
 class TemporalQueryUtilTest extends org.scalatest.FunSuite {
   test("plusMillisecond") {
@@ -48,8 +48,35 @@ class TemporalQueryUtilTest extends org.scalatest.FunSuite {
       StructField("Wert_B", DoubleType),
       StructField("gueltig_ab", TimestampType),
       StructField("gueltig_bis", TimestampType)))
-    val rowsExpected = List(Row(0,97.15,defaultConfig.minDate,Timestamp.valueOf("2018-10-23 03:50:09.999")),
-      Row(0,97.15,Timestamp.valueOf("2018-10-23 03:50:10.000"),defaultConfig.maxDate)).asJava
+    val rowsExpected = List(
+      Row(0,97.15,defaultConfig.minDate                     ,Timestamp.valueOf("2018-01-31 23:59:59.999")),
+      Row(0,97.15,Timestamp.valueOf("2018-06-01 05:24:11.0"),Timestamp.valueOf("2018-10-23 03:50:09.999")),
+      Row(0,97.15,Timestamp.valueOf("2018-10-23 03:50:10.0"),defaultConfig.maxDate),
+      Row(1, null,defaultConfig.minDate                     ,Timestamp.valueOf("2018-12-31 23:59:59.999")),
+      Row(1, null,Timestamp.valueOf("2018-10-23 00:00:00.0"),defaultConfig.maxDate))
+      .asJava
+    val expected: DataFrame = ss.createDataFrame(rows=rowsExpected, schemaExpected).orderBy("Id","gueltig_ab")
+    val expectedWithArgumentColumns: DataFrame = expected.select(actual.columns.map(col):_*)
+    val resultat: Boolean = dfEqual(actual)(expectedWithArgumentColumns)
+
+    if (!resultat)printFailedTestResult("temporalExtendRange2")(dfRight)(actual)(expected)
+    assert(resultat)
+  }
+
+  test("temporalCombine") {
+    // argument: dfRight from object TestUtils
+    val actual: DataFrame = dfRight.temporalCombine(Seq("id"))(ss,defaultConfig)
+
+    val schemaExpected = StructType(Array(StructField("id", IntegerType),
+      StructField("Wert_B", DoubleType),
+      StructField("gueltig_ab", TimestampType),
+      StructField("gueltig_bis", TimestampType)))
+    val rowsExpected = List(
+      Row(0,97.15,Timestamp.valueOf("2018-01-01 00:00:00.0"),Timestamp.valueOf("2018-01-31 23:59:59.999")),
+      Row(0,97.15,Timestamp.valueOf("2018-06-01 05:24:11.0"),Timestamp.valueOf("9999-12-31 23:59:59.999")),
+      Row(1, null,Timestamp.valueOf("2018-01-01 00:00:00.0"),Timestamp.valueOf("2018-12-31 23:59:59.999")),
+      Row(1, null,Timestamp.valueOf("2018-10-23 00:00:00.0"),Timestamp.valueOf("2019-12-31 23:59:59.999")))
+      .asJava
     val expected: DataFrame = ss.createDataFrame(rows=rowsExpected, schemaExpected).orderBy("Id","gueltig_ab")
     val expectedWithArgumentColumns: DataFrame = expected.select(actual.columns.map(col):_*)
     val resultat: Boolean = dfEqual(actual)(expectedWithArgumentColumns)
--- src/test/scala/ch.zzeekk.spark.temporalquery/TestUtils.scala
+++ src/test/scala/ch.zzeekk.spark.temporalquery/TestUtils.scala
@@ -1,11 +1,11 @@
 package ch.zzeekk.spark.temporalquery
 
-import java.sql.Timestamp
 import ch.zzeekk.spark.temporalquery.TemporalQueryUtil.TemporalQueryConfig
+import java.sql.Timestamp
 import org.apache.spark.sql._
+import org.apache.spark.sql.functions.lit
 import org.apache.spark.sql.types.{DoubleType, IntegerType, StructField, StructType, TimestampType}
 import scala.collection.JavaConverters._
-
 
 object TestUtils {
   implicit val ss: SparkSession = SparkSession.builder.master("local").appName("TemporalQueryUtilTest").getOrCreate()
@@ -18,20 +18,29 @@ object TestUtils {
     StructField("gueltig_ab", TimestampType),
     StructField("gueltig_bis", TimestampType),
     StructField("Wert_A", DoubleType)))
-  val rowsLeft = List((0,"2017-12-10 00:00:00.000","2018-12-08 23:59:59.999",4.2))
+  val rowsLeft = List((0,"2017-12-10 00:00:00.0","2018-12-08 23:59:59.999",4.2))
   val dfLeft: DataFrame = ss.createDataFrame(rows=rowsLeft.map(makeRow).asJava, schemaLeft).orderBy("Id","gueltig_ab")
 
   val schemaRight = StructType(Array(StructField("id", IntegerType),
     StructField("gueltig_ab", TimestampType),
     StructField("gueltig_bis", TimestampType),
     StructField("Wert_B", DoubleType)))
-  val rowsRight = List(
-    (0,"2018-06-01 05:24:11.000","2018-10-23 03:50:09.999",97.15),
-    (0,"2018-10-23 03:50:10.000","9999-12-31 23:59:59.999",97.15))
-  val dfRight: DataFrame = ss.createDataFrame(rows=rowsRight.map(makeRow).asJava, schemaRight)
+  val rowList = List(
+    (0,"2018-01-01 00:00:00.0","2018-01-31 23:59:59.999",97.15),
+    // gap in history
+    (0,"2018-06-01 05:24:11.0","2018-10-23 03:50:09.999",97.15),
+    (0,"2018-10-23 03:50:10.0","9999-12-31 23:59:59.999",97.15))
+  val rowsRight: List[Row] = rowList.map(makeRow) :+ Row(1,Timestamp.valueOf("2018-01-01 00:00:00.0"),Timestamp.valueOf("2018-12-31 23:59:59.999"),null) :+ Row(1,Timestamp.valueOf("2018-10-23 00:00:00.0"),Timestamp.valueOf("2019-12-31 23:59:59.999"),null)
+
+  val dfRight: DataFrame = ss.createDataFrame(rows=rowsRight.asJava, schemaRight)
+
+  def symmetricDifference(df1: DataFrame)(df2: DataFrame): DataFrame = {
+    df1.except(df2).withColumn("_df",lit(1)).union(df2.except(df1).withColumn("_df",lit(2)))
+  }
 
   def dfEqual(df1: DataFrame)(df2: DataFrame): Boolean = {
-    0 == df1.except(df2).union(df2.except(df1)).count
+    // symmetricDifference ignoriert Doubletten, daher Kardinalitäten vergleichen
+    (0 == symmetricDifference(df1)(df2).count) && (df1.count == df2.count)
   }
 
   def printFailedTestResult(testName: String)(argument: DataFrame)(actual: DataFrame)(expected: DataFrame): Unit = {
@@ -42,5 +51,8 @@ object TestUtils {
     actual.show(false)
     println("   expected "+expected.schema.simpleString)
     expected.show(false)
+    println("   symmetricDifference ")
+    symmetricDifference(actual)(expected).show(false)
   }
+
 }
