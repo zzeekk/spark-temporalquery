98de57e7f136e6590587ab2a084e0d9d23a45d3c
Nikolaus Thiel <nikolaus.thiel@sbb.ch>
2019-09-23 09:41:00 +0200

    2 test cases for temporalLeftJoin

--- src/test/scala/ch.zzeekk.spark.temporalquery/TemporalQueryUtilTest.scala
+++ src/test/scala/ch.zzeekk.spark.temporalquery/TemporalQueryUtilTest.scala
@@ -28,11 +28,11 @@ class TemporalQueryUtilTest extends org.scalatest.FunSuite {
     val actual: DataFrame = dfLeft.temporalExtendRange(Seq("id"))(ss,defaultConfig)
 
     val schemaExpected = StructType(Array(StructField("id", IntegerType),
-      StructField("Wert_A", DoubleType),
-      StructField("gueltig_ab", TimestampType),
-      StructField("gueltig_bis", TimestampType)))
+      StructField("Wert_L", DoubleType),
+      StructField(defaultConfig.fromColName, TimestampType),
+      StructField(defaultConfig.toColName, TimestampType)))
     val rowsExpected = List(Row(0,4.2,defaultConfig.minDate,defaultConfig.maxDate)).asJava
-    val expected: DataFrame = ss.createDataFrame(rows=rowsExpected, schemaExpected).orderBy("Id","gueltig_ab")
+    val expected: DataFrame = ss.createDataFrame(rowsExpected, schemaExpected).orderBy("Id",defaultConfig.fromColName)
     val expectedWithArgumentColumns: DataFrame = expected.select(actual.columns.map(col):_*)
     val resultat: Boolean = dfEqual(actual)(expectedWithArgumentColumns)
 
@@ -45,9 +45,9 @@ class TemporalQueryUtilTest extends org.scalatest.FunSuite {
     val actual: DataFrame = dfRight.temporalExtendRange(Seq("id"))(ss,defaultConfig)
 
     val schemaExpected = StructType(Array(StructField("id", IntegerType),
-      StructField("Wert_B", DoubleType),
-      StructField("gueltig_ab", TimestampType),
-      StructField("gueltig_bis", TimestampType)))
+      StructField("Wert_R", DoubleType),
+      StructField(defaultConfig.fromColName, TimestampType),
+      StructField(defaultConfig.toColName, TimestampType)))
     val rowsExpected = List(
       Row(0,97.15,defaultConfig.minDate                     ,Timestamp.valueOf("2018-01-31 23:59:59.999")),
       Row(0,97.15,Timestamp.valueOf("2018-06-01 05:24:11.0"),Timestamp.valueOf("2018-10-23 03:50:09.999")),
@@ -55,11 +55,54 @@ class TemporalQueryUtilTest extends org.scalatest.FunSuite {
       Row(1, null,defaultConfig.minDate                     ,Timestamp.valueOf("2018-12-31 23:59:59.999")),
       Row(1, null,Timestamp.valueOf("2018-10-23 00:00:00.0"),defaultConfig.maxDate))
       .asJava
-    val expected: DataFrame = ss.createDataFrame(rows=rowsExpected, schemaExpected).orderBy("Id","gueltig_ab")
+    val expected: DataFrame = ss.createDataFrame(rowsExpected, schemaExpected).orderBy("Id",defaultConfig.fromColName)
     val expectedWithArgumentColumns: DataFrame = expected.select(actual.columns.map(col):_*)
     val resultat: Boolean = dfEqual(actual)(expectedWithArgumentColumns)
 
     if (!resultat)printFailedTestResult("temporalExtendRange2")(dfRight)(actual)(expected)
+    assert(resultat)
+  }
+
+  test("temporalLeftJoin1") {
+    val actual: DataFrame = dfLeft.temporalLeftJoin(dfRight,Seq("id"),Seq(col(defaultConfig.fromColName)))(ss,defaultConfig)
+    val schemaExpected = StructType(Array(StructField("id", IntegerType),
+      StructField("Wert_L", DoubleType),
+      StructField("Wert_R", DoubleType),
+      StructField(defaultConfig.fromColName, TimestampType),
+      StructField(defaultConfig.toColName, TimestampType)))
+    val rowsExpected = List(
+      Row(0,4.2, null,Timestamp.valueOf("2017-12-10 00:00:00"),Timestamp.valueOf("2017-12-31 23:59:59.999")),
+      Row(0,4.2,97.15,Timestamp.valueOf("2018-01-01 00:00:00"),Timestamp.valueOf("2018-01-31 23:59:59.999")),
+      Row(0,4.2, null,Timestamp.valueOf("2018-02-01 00:00:00"),Timestamp.valueOf("2018-06-01 05:24:10.999")),
+      Row(0,4.2,97.15,Timestamp.valueOf("2018-06-01 05:24:11"),Timestamp.valueOf("2018-10-23 03:50:09.999")),
+      Row(0,4.2,97.15,Timestamp.valueOf("2018-10-23 03:50:10"),Timestamp.valueOf("2018-12-08 23:59:59.999")))
+      .asJava
+    val expected: DataFrame = ss.createDataFrame(rowsExpected, schemaExpected).orderBy("Id",defaultConfig.fromColName)
+    val resultat: Boolean = dfEqual(actual)(expected)
+
+    if (!resultat)printFailedTestResult("temporalLeftJoin1")(dfRight)(actual)(expected)
+    assert(resultat)
+  }
+
+  test("temporalLeftJoin2") {
+    val actual: DataFrame = dfLeft.temporalLeftJoin(dfMap,Seq("id"),Seq(col(defaultConfig.fromColName)))(ss,defaultConfig)
+    val schemaExpected = StructType(Array(StructField("id", IntegerType),
+      StructField("Wert_L", DoubleType),
+      StructField("img", StringType),
+      StructField(defaultConfig.fromColName, TimestampType),
+      StructField(defaultConfig.toColName, TimestampType)))
+    val rowsExpected = List(
+      Row(0,4.2,null,Timestamp.valueOf("2017-12-10 00:00:00"),Timestamp.valueOf("2017-12-31 23:59:59.999")),
+      Row(0,4.2, "A",Timestamp.valueOf("2018-01-01 00:00:00"),Timestamp.valueOf("2018-01-31 23:59:59.999")),
+      Row(0,4.2, "B",Timestamp.valueOf("2018-01-01 00:00:00"),Timestamp.valueOf("2018-02-28 23:59:59.999")),
+      Row(0,4.2, "C",Timestamp.valueOf("2018-02-01 00:00:00"),Timestamp.valueOf("2018-02-28 23:59:59.999")),
+      Row(0,4.2, "D",Timestamp.valueOf("2018-03-01 00:00:00"),Timestamp.valueOf("2018-03-31 23:59:59.999")),
+      Row(0,4.2,null,Timestamp.valueOf("2018-04-01 00:00:00"),Timestamp.valueOf("2018-12-08 23:59:59.999")))
+      .asJava
+    val expected: DataFrame = ss.createDataFrame(rowsExpected, schemaExpected).orderBy("Id",defaultConfig.fromColName)
+    val resultat: Boolean = dfEqual(actual)(expected)
+
+    if (!resultat)printFailedTestResult("temporalLeftJoin2")(dfRight)(actual)(expected)
     assert(resultat)
   }
 
@@ -68,16 +111,16 @@ class TemporalQueryUtilTest extends org.scalatest.FunSuite {
     val actual: DataFrame = dfRight.temporalCombine(Seq("id"))(ss,defaultConfig)
 
     val schemaExpected = StructType(Array(StructField("id", IntegerType),
-      StructField("Wert_B", DoubleType),
-      StructField("gueltig_ab", TimestampType),
-      StructField("gueltig_bis", TimestampType)))
+      StructField("Wert_R", DoubleType),
+      StructField(defaultConfig.fromColName, TimestampType),
+      StructField(defaultConfig.toColName, TimestampType)))
     val rowsExpected = List(
       Row(0,97.15,Timestamp.valueOf("2018-01-01 00:00:00.0"),Timestamp.valueOf("2018-01-31 23:59:59.999")),
       Row(0,97.15,Timestamp.valueOf("2018-06-01 05:24:11.0"),Timestamp.valueOf("9999-12-31 23:59:59.999")),
       Row(1, null,Timestamp.valueOf("2018-01-01 00:00:00.0"),Timestamp.valueOf("2018-12-31 23:59:59.999")),
       Row(1, null,Timestamp.valueOf("2018-10-23 00:00:00.0"),Timestamp.valueOf("2019-12-31 23:59:59.999")))
       .asJava
-    val expected: DataFrame = ss.createDataFrame(rows=rowsExpected, schemaExpected).orderBy("Id","gueltig_ab")
+    val expected: DataFrame = ss.createDataFrame(rowsExpected, schemaExpected).orderBy("Id",defaultConfig.fromColName)
     val expectedWithArgumentColumns: DataFrame = expected.select(actual.columns.map(col):_*)
     val resultat: Boolean = dfEqual(actual)(expectedWithArgumentColumns)
 
--- src/test/scala/ch.zzeekk.spark.temporalquery/TestUtils.scala
+++ src/test/scala/ch.zzeekk.spark.temporalquery/TestUtils.scala
@@ -4,7 +4,7 @@ import ch.zzeekk.spark.temporalquery.TemporalQueryUtil.TemporalQueryConfig
 import java.sql.Timestamp
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions.lit
-import org.apache.spark.sql.types.{DoubleType, IntegerType, StructField, StructType, TimestampType}
+import org.apache.spark.sql.types._
 import scala.collection.JavaConverters._
 
 object TestUtils {
@@ -15,24 +15,36 @@ object TestUtils {
   def makeRow(zeile: Tuple4[Int, String, String, Double]): Row = Row(zeile._1,Timestamp.valueOf(zeile._2),Timestamp.valueOf(zeile._3),zeile._4)
 
   val schemaLeft = StructType(Array(StructField("id", IntegerType),
-    StructField("gueltig_ab", TimestampType),
-    StructField("gueltig_bis", TimestampType),
-    StructField("Wert_A", DoubleType)))
-  val rowsLeft = List((0,"2017-12-10 00:00:00.0","2018-12-08 23:59:59.999",4.2))
-  val dfLeft: DataFrame = ss.createDataFrame(rows=rowsLeft.map(makeRow).asJava, schemaLeft).orderBy("Id","gueltig_ab")
+    StructField(defaultConfig.fromColName, TimestampType),
+    StructField(defaultConfig.toColName, TimestampType),
+    StructField("Wert_L", DoubleType)))
+  val rowsLeft = List((0,"2017-12-10 00:00:00","2018-12-08 23:59:59.999",4.2))
+  val dfLeft: DataFrame = ss.createDataFrame(rows=rowsLeft.map(makeRow).asJava, schemaLeft).orderBy("Id",defaultConfig.fromColName)
 
   val schemaRight = StructType(Array(StructField("id", IntegerType),
-    StructField("gueltig_ab", TimestampType),
-    StructField("gueltig_bis", TimestampType),
-    StructField("Wert_B", DoubleType)))
-  val rowList = List(
-    (0,"2018-01-01 00:00:00.0","2018-01-31 23:59:59.999",97.15),
+    StructField(defaultConfig.fromColName, TimestampType),
+    StructField(defaultConfig.toColName, TimestampType),
+    StructField("Wert_R", DoubleType)))
+  val rowListRight = List(
+    (0,"2018-01-01 00:00:00","2018-01-31 23:59:59.999",97.15),
     // gap in history
-    (0,"2018-06-01 05:24:11.0","2018-10-23 03:50:09.999",97.15),
-    (0,"2018-10-23 03:50:10.0","9999-12-31 23:59:59.999",97.15))
-  val rowsRight: List[Row] = rowList.map(makeRow) :+ Row(1,Timestamp.valueOf("2018-01-01 00:00:00.0"),Timestamp.valueOf("2018-12-31 23:59:59.999"),null) :+ Row(1,Timestamp.valueOf("2018-10-23 00:00:00.0"),Timestamp.valueOf("2019-12-31 23:59:59.999"),null)
-
+    (0,"2018-06-01 05:24:11","2018-10-23 03:50:09.999",97.15),
+    (0,"2018-10-23 03:50:10","9999-12-31 23:59:59.999",97.15))
+  val rowsRight: List[Row] = rowListRight.map(makeRow) :+
+    Row(1,Timestamp.valueOf("2018-01-01 00:00:00"),Timestamp.valueOf("2018-12-31 23:59:59.999"),null) :+
+    Row(1,Timestamp.valueOf("2018-10-23 00:00:00"),Timestamp.valueOf("2019-12-31 23:59:59.999"),null)
   val dfRight: DataFrame = ss.createDataFrame(rows=rowsRight.asJava, schemaRight)
+
+  val schemaMap = StructType(Array(StructField("id", IntegerType),
+    StructField(defaultConfig.fromColName, TimestampType),
+    StructField(defaultConfig.toColName, TimestampType),
+    StructField("img", StringType)))
+  val rowsMap: List[Row] = List(
+    Row(1,Timestamp.valueOf("2018-01-01 00:00:00"),Timestamp.valueOf("2018-01-31 23:59:59.999"),"A"),
+    Row(1,Timestamp.valueOf("2018-01-01 00:00:00"),Timestamp.valueOf("2018-02-28 23:59:59.999"),"B"),
+    Row(1,Timestamp.valueOf("2018-02-01 00:00:00"),Timestamp.valueOf("2018-02-28 23:59:59.999"),"C"),
+    Row(1,Timestamp.valueOf("2018-03-01 00:00:00"),Timestamp.valueOf("2018-03-31 23:59:59.999"),"D"))
+  val dfMap: DataFrame = ss.createDataFrame(rows=rowsMap.asJava, schemaMap)
 
   def symmetricDifference(df1: DataFrame)(df2: DataFrame): DataFrame = {
     df1.except(df2).withColumn("_df",lit(1)).union(df2.except(df1).withColumn("_df",lit(2)))
