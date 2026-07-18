b5004c17beac51dc189be22a850a941e6cca6c55
Nikolaus Thiel <klt@fsfe.org>
2026-07-18 18:14:49 +0200

    BiTemporalQueryUtil defined but not tested (yet)

---
+++ src/main/scala/ch/zzeekk/spark/temporalquery/util/BiTemporalQueryUtil.scala
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2017 Zacharias Kull under MIT Licence
+ */
+
+package ch.zzeekk.spark.temporalquery.util
+
+import ch.zzeekk.spark.temporalquery._
+
+import java.sql.Timestamp
+
+/**
+ * Temporal query utils for interval axis of type Timestamp
+ *
+ * Usage: import ch.zzeekk.spark.temporalquery.TemporalQueryUtil._ // this imports temporal*
+ * implicit functions on DataFrame and Columns implicit val tqc = TemporalQueryConfig() // configure
+ * options for temporal query operations implicit val sss = ss // make SparkSession implicitly
+ * available val df_joined = df1.temporalJoin(df2) // use temporal query functions with Spark
+ */
+object BiTemporalQueryUtil extends Serializable with Logging {
+
+  /**
+   * Trait to mark bitemporal query configurations to make implicit resolution unique if there is
+   * also an implicit linear query configuration in scope
+   */
+  trait BiTemporalQueryConfigMarker
+
+  /**
+   * Type which includes BiTemporalClosedIntervalQueryConfig and
+   * BiTemporalHalfOpenIntervalQueryConfig
+   */
+  type BiTemporalQueryConfig = IntervalMultidimQueryConfig[Timestamp, _] with BiTemporalQueryConfigMarker
+
+  /**
+   * Configuration Parameters. An instance of this class is needed as implicit parameter.
+   */
+  case class BiTemporalClosedIntervalQueryConfig(
+      override val dimensionMap: Map[String, (String, ClosedInterval[Timestamp])] =
+        Map("known_from" -> ("known_to", stdClosedTemporalInterval),
+          "valid_from"   -> ("valid_to", stdClosedTemporalInterval)),
+      override val additionalTechnicalColNames: Seq[String] = Nil
+  ) extends ClosedIntervalMultidimQueryConfig[Timestamp] with BiTemporalQueryConfigMarker {
+    override def config2: BiTemporalClosedIntervalQueryConfig = this
+      .copy(dimensionMap = dimensionMap.map { case (f, (t, i)) => (increaseColNameNb(f), (increaseColNameNb(t), i)) })
+  }
+
+  /**
+   * Configuration Parameters for operations on temporal interval axis. An instance of this class is
+   * needed as implicit parameter for all temporal query functions.
+   */
+  case class BiTemporalHalfOpenIntervalQueryConfig(
+      override val dimensionMap: Map[String, (String, HalfOpenInterval[Timestamp])] =
+        Map("known_from" -> ("known_to", HalfOpenInterval(bigBangDay, doomsDay)),
+          "valid_from"   -> ("valid_to", HalfOpenInterval(bigBangDay, doomsDay))),
+      override val additionalTechnicalColNames: Seq[String] = Nil
+  ) extends HalfOpenIntervalMultidimQueryConfig[Timestamp] with BiTemporalQueryConfigMarker {
+    override def config2: BiTemporalHalfOpenIntervalQueryConfig = this
+      .copy(dimensionMap = dimensionMap.map { case (f, (t, i)) => (increaseColNameNb(f), (increaseColNameNb(t), i)) })
+  }
+}
